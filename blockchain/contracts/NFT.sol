b75d10117a2e1c9ab18d433a0d09b393242c46be
Swahim Namdev <swahimn@gmail.com>
2022-07-18 23:10:27 +0530

    storing tokenId

--- blockchain/contracts/NFT.sol
+++ blockchain/contracts/NFT.sol
@@ -9,7 +9,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
-    mapping(uint256 => string) public tokenIdToSerialNumber;
+    mapping(string => uint256) public serialNumberToTokenId; //To store NFT associated with the serial number of the product
     mapping(string => string) public serialNumberToIPFSHash; //As the image will be uploaded before the NFT is minted so we have to store the image for the product in the mapping
     Counters.Counter private _tokenIdCounter;
 
@@ -20,7 +20,7 @@ contract NFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
-        tokenIdToSerialNumber[tokenId] = _serialNumber;
+        serialNumberToTokenId[_serialNumber] = tokenId;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
