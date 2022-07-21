b590413fd69f7094375f0a7ea8d83cc4e6e2498b
Swahim Namdev <swahimn@gmail.com>
2022-07-21 13:14:41 +0530

    updates

--- client/src/components/productCard/card.jsx
+++ client/src/components/productCard/card.jsx
@@ -3,7 +3,7 @@ const Card = (props) => {
   return (
     <div className="cardContainer">
       <div className="cardImage">
-        <img src="https://www.mydesignation.com/wp-content/uploads/2019/08/malayali-tshirt-mydesignation-mockup-image-latest-golden-.jpg" />
+        <img src={props.image} />
       </div>
       <div>
         <h3>{props.productName}</h3>
@@ -14,3 +14,6 @@ const Card = (props) => {
 };
 
 export default Card;
+
+//https://media.istockphoto.com/photos/stylish-blue-headphones-on-multi-colored-duo-tone-background-lighting-picture-id1175355990?k=20&m=1175355990&s=612x612&w=0&h=LX5kcpZKWyJQA_Kh5Ub9EwDNpGtAimGr2AePNQJPYxE=
+//https://www.mydesignation.com/wp-content/uploads/2019/08/malayali-tshirt-mydesignation-mockup-image-latest-golden-.jpg
